78a06918fb8f3a1e8d542d908106e0355205589f
ilhmdhn <doha39anilham@gmail.com>
2022-11-14 22:32:33 +0700

    bikin query

--- src/model/sliporder-data.js
+++ src/model/sliporder-data.js
@@ -16,7 +16,7 @@ VALUES
 ,CONVERT(VARCHAR(24),GETDATE(),103) + ' '+ SUBSTRING(CONVERT(VARCHAR(24),GETDATE(),114),1,8)
 ,'${shift}'
 ,'${rcp}'
-" '${roomCode}'
+,'${roomCode}'
 ,'1'
 ,CONVERT(VARCHAR(24),GETDATE(),103) + ' '+ SUBSTRING(CONVERT(VARCHAR(24),GETDATE(),114),1,8)
 '${chusr}'
@@ -24,42 +24,42 @@ VALUES
 ,${date_trans_Query}
 ,'${android}')`;
 
-var insert_sod_query = " " +
-            " INSERT INTO IHP_Sod " +
-            " (SlipOrder, " +
-            " Inventory, " +
-            " Nama, " +
-            " Price, " +
-            " Qty, " +
-            " Total, " +
-            " Status, " +
-            " Location, " +
-            " Printed, " +
-            " Note, " +
-            " CHusr, " +
-            " Urut) " +
-            " VALUES " +
-            " ('" + kode_sol + "'," +
-            " '" + arrOrderInv[a] + "'," +
-            " '" + arrOrderInvName[a] + "'," +
-            " '" + parseFloat(arrOrderPrice[a]) + "'," +
-            " '" + arrOrderQty[a] + "'," +
-            " '" + parseFloat((arrOrderPrice[a] * arrOrderQty[a])) + "'," +
-            " '1'," +
-            " '" + arrOrderInvLocation[a] + "'," +
-            " '2'," +
-            " '" + arrOrderNotes[a] + "'," +
-            " '" + chusr + "'," +
-            " " + parseInt(a) + ")";
+var insert_sod_query = 
+`INSERT INTO IHP_Sod
+([SlipOrder],
+[Inventory],
+[Nama],
+[Price],
+[Qty],
+[Total],
+[Status],
+[Location],
+[Printed],
+[Note],
+[CHusr],
+[Urut])
+VALUES
+('${kode_sol}',
+'${arrOrderInv[a]}',
+'${arrOrderInvName[a]}',
+'${parseFloat(arrOrderPrice[a])}',
+'${arrOrderQty[a]}',
+'${parseFloat((arrOrderPrice[a] * arrOrderQty[a]))}',
+'1',
+'${arrOrderInvLocation[a]}',
+'2',
+'${arrOrderNotes[a]}',
+'${chusr}',
+${parseInt(a)})`;
 
-var insert_sod_promo_query = " " +
-" INSERT INTO IHP_Sod_Promo " +
-" (SlipOrder, " +
-" Inventory, " +
-" Promo_Food, " +
-" Harga_Promo) " +
-" VALUES " +
-" ('" + kode_sol + "'," +
+var insert_sod_promo_query =`
+INSERT INTO IHP_Sod_Promo " +
+([SlipOrder],
+[Inventory],
+[Promo_Food],
+[Harga_Promo]) " +
+VALUES
+('kode_sol + "'," +
 " '" + arrOrderInv[a] + "'," +
 " '" + nama_promo + "'," +
 " '" + parseFloat((arrFinal_Potongan_Promo[a] * arrOrderQty[a])) + "')";
