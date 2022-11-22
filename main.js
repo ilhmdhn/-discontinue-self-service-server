041fd48fa47f0e71dc0b831712f021d770433957
ilhmdhn <doha39anilham@gmail.com>
2022-11-22 11:47:04 +0700

    fix window

--- main.js
+++ main.js
@@ -1,75 +1,56 @@
 const { app, BrowserWindow, Tray } = require('electron');
 const fs = require('fs');
 const setup = JSON.parse(fs.readFileSync('setup.json'));
-const path = require('path')
-
+const logger = require('./src/util/logger');
 
 const port = setup.server_port;
 function createWindow () {
     app.server = require(__dirname+'/src/index.js', (error)=>{
-
-      const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
-        }
-      });
-      win.webContents.session.clearStorageData();
-      win.webContents.session.clearCache(() =>{
-        console.log("clear cache");
-      })
-    
-    
-      if(error){
-        
-      }else{
-        win.loadURL(`http://localhost:${port}`);
-        win.focus();
-        win.center();
-
-        win.on('closed', (event)=>{
-          win == null;
-        })
-
-        win.on('minimize', (event) =>{
-          event.preventDefault();
-          win.hide();
-        });
-
-        win.on('close', (event) =>{
-          if(!app.isQuiting){
-            event.preventDefault()
-            win.hide()
-          }
-          return false;
-        })
-
-        const tray = new Tray('icon')
-        tray.on('click', ()=>{
-          if(win.isVisible()){
-            win.hide();
-          }else{
-            win.show();
-          }
-        })
-        tray.setTitle('Server Self Service');
+      if(err){
+        logger.error(`Fail to open /src/index.js`);
       }
     });  
-}
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    });
+
+    win.webContents.session.clearStorageData();
+    win.webContents.session.clearCache(() =>{
+      console.log("clear cache");
+    })
+  
+      win.loadURL(`http://localhost:${port}`);
+      win.focus();
+      win.center();
+
+      win.on('closed', (event)=>{
+        win == null;
+        app.quit();
+      })
+
+      win.on('minimize', (event) =>{
+        event.preventDefault();
+        win.hide();
+      });
+
+      win.on('close', (event) =>{
+          win == null;
+          app.quit();
+      })
+
+    const tray = new Tray('icon.png');
+      tray.on('click', ()=>{
+        if(win.isVisible()){
+          win.hide();
+        }else{
+          win.show();
+        }
+      })
+    tray.setTitle('Self Service');
+  }
+
 
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  createWindow();
+});
